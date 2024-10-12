9c0dfbd659ee290824a8b4ca014605fe80c436c7
intisor <abdulawwalintisor777@gmail.com>
2024-10-12 21:39:39 +0100

    fixed bugs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
+using YoutubeExplode;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSingleton<YoutubeClient>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
